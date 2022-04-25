77c377aa4a7245c54550e145b954149ceaa8a0b4
Prathamesh Belurkar <76102350+prathameshbelurkar@users.noreply.github.com>
2022-04-25 14:23:44 +0530

    Linked DB

---
+++ MongoDB.md
@@ -0,0 +1,31 @@
+# MongoDB Notes 🧑‍🏫
+
+<hr>
+
+#### 1. Use [database_name]
+
+- Get switched to the Database newly created database 'database_name'
+
+#### 2. db.collection_name.insertOne()
+
+#### 3. db.collection_name.insertMany()
+
+#### 4. db.tours.find()
+
+#### 5. show dbs
+
+#### 6. Show collections
+
+#### 7. quit()
+
+<hr>
+
+## CRUD Operations
+
+#### 2. db.
+
+> db.tours.updateOne()
+
+> db.tours.replaceOne()
+
+> db.tours.deleteMany()
--- server.js
+++ server.js
@@ -1,7 +1,27 @@
+const mongoose = require('mongoose');
 const dotenv = require('dotenv');
 
 // Accessing ENV variables
 dotenv.config({ path: './config.env' });
+
+// Getting connect link from env variables
+const DB = process.env.DATABASE.replace(
+  '<PASSWORD>',
+  process.env.DATABASE_PASSWORD
+);
+
+/*
+Connecting to mongodb via mongoose: returns a Promise
+Remember 📝
+when connecting to local mongodb database use ```.connect(process.env.DATABASE_LOCAL)
+*/
+mongoose
+  .connect(DB, {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useFindAndModify: false,
+  })
+  .then(() => console.log('DB Connection successful!'));
 
 // Importing app file
 const app = require('./app');
