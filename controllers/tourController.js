c1a641dbeddfda8b7f27d3027892e86f678ddc72
Prathamesh Belurkar <76102350+prathameshbelurkar@users.noreply.github.com>
2022-04-26 08:53:47 +0530

    Updated

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -56,7 +56,7 @@ exports.createTour = async (req, res) => {
   } catch (err) {
     res.status(400).json({
       status: 'failed',
-      message: 'Invalid data sent!',
+      message: err,
     });
   }
 };
@@ -84,9 +84,18 @@ exports.updateTour = async (req, res) => {
 };
 
 // DELETE
-exports.deleteTour = (req, res) => {
-  res.status(204).json({
-    status: 'success',
-    data: null,
-  });
+exports.deleteTour = async (req, res) => {
+  try {
+    await Tour.findByIdAndDelete(req.params.id);
+
+    res.status(204).json({
+      status: 'success',
+      data: null,
+    });
+  } catch (err) {
+    res.status(404).json({
+      status: 'fail',
+      message: err,
+    });
+  }
 };
